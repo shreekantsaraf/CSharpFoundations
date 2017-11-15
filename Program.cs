c2e1414d4b56bf3bbb34abc8a8958a393f269e12
Bugs <svsaraf@yahoo.com>
2017-11-15 12:18:27 -0800

    first-checkin
    first-checkin

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpFoundations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, Please enter your name - ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + ",  welcome to CASharpeFoundations 100 course");
+        }
+    }
+}
