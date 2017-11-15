4f1b3a974080d6a425e4e95e59b2f553e34f6494
Bugs <svsaraf@yahoo.com>
2017-11-15 13:33:15 -0800

    c#2
    lesson 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿/*Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number 
+and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+ */
+using System;
 
 namespace CSharpFoundations
 {
@@ -6,9 +9,18 @@ namespace CSharpFoundations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello, Please enter your name - ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello " + name + ",  welcome to CASharpeFoundations 100 course");
+            Console.WriteLine("hello, Please enter number between 5 and 20 - ");
+            int number = int.Parse(Console.ReadLine());
+            while (number > 0) 
+            {
+                Console.Write(number + " ");
+                if (number % 3 == 0) Console.Write("Fizz");
+                if (number % 5 == 0) Console.Write("Buzz");
+                Console.WriteLine();
+                number= number-1;
+            }
+            Console.WriteLine("done. press any key");
+            Console.ReadKey();
         }
     }
 }
